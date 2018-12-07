8457594ce6f9461bc6d30c82197c5fad2bc4f1a6
At Indo <emailta.indo@gmail.com>
2018-12-07 14:28:01 +0800

    Improve app style

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
+<img src="https://github.com/athif23/start-imba/blob/master/logo/imba-logo.png" alt="drawing" width="200"/>
+
 # start-imba
-A generator to create new imba workspace
+
+A generator to create new [Imba](https://github.com/imba/imba) workspace
+
+> This project is still in development. If you really want to use it follow commands below:
+
+#### How to use it:
+
+> Make sure you already have NodeJS installed on your pc.
+
+- Open your terminal.
+
+- Install the dependencies `npm install`
+
+- Install the app globally `npm install -g ./`
+
+- Now you can run, `start-imba <yourappname>`
+
+- Then, select bundler you will use. For now, its `webpack` and `parcel`
+
+- Done.
+
+#### Things to do:
+
+- Create `with-parcel` template.
+
+- Create more option for the app, so people can choose tools they want to use with imba.
+
+- Make the app more cool and fast.
--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const program = require('commander');
+#!/usr/bin/env node
+const commander = require('commander');
 const enquirer = require('enquirer');
+const chalk = require('chalk');
 const fs = require('fs');
 const CURR_DIR = process.cwd();
+const execSync = require('child_process').execSync;
+const packageJson = require('./package.json');
 
 const CHOICES = fs.readdirSync(`${__dirname}/templates`);
 const QUESTIONS = [
@@ -10,33 +14,29 @@ const QUESTIONS = [
     type: 'select',
     message: 'What bundler you would like to use?',
     choices: CHOICES
-  },
-  {
-    name: 'project-name',
-    type: 'input',
-    message: 'Project name:',
-    validate: function (input) {
-      if (/^([A-Za-z\-\_\d])+$/.test(input)) return true;
-      else return 'Project name may only include letters, numbers, underscores and hashes.';
-    }
   }
 ];
 
-program
-  .command('create')
-  .action(function () {
-  	enquirer.prompt(QUESTIONS)
-	  .then(answers => {
-	    const projectChoice = answers['bundler'];
-	    const projectName = answers['project-name'];
-	    const templatePath = `${__dirname}/templates/${projectChoice}`;
-	  
-	    fs.mkdirSync(`${CURR_DIR}/${projectName}`);
+const program = new commander.Command(packageJson.name)
+.version(packageJson.version)
+.description("A generator to create new Imba workspace")
+.arguments('<project-directory>')
+.usage(`${chalk.green('<project-directory>')} [options]`)
+.action(function (name) {
+    enquirer.prompt(QUESTIONS)
+    .then(answers => {
+      const projectChoice = answers['bundler'];
+      const projectName = name;
+      const templatePath = `${__dirname}/templates/${projectChoice}`;
+    
+      fs.mkdirSync(`${CURR_DIR}/${projectName}`);
 
-	    createDirectoryContents(templatePath, projectName);
-	});
-  })
-
+      createDirectoryContents(templatePath, projectName);
+      console.log();
+      console.info('Your Imba project has been created! Now, go make something fast!');
+      console.info('\n');
+     });
+  });
 
 function createDirectoryContents (templatePath, newProjectPath) {
   const filesToCreate = fs.readdirSync(templatePath);
